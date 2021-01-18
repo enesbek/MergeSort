8d7dcb525ca77f9957f6a9e50b5fd18ab1446845
ENES BEK <enes.bek@stu.fsm.edu.tr>
2021-01-18 15:47:06 +0300

    Update Test.java

--- MergeSort/src/mergesort/Test.java
+++ MergeSort/src/mergesort/Test.java
@@ -12,33 +12,34 @@ public class Test {
         
         int[] myArray = new int[x];
         int[] myArray2 = new int[x];
-
-        Random rand = new Random(11);
+        int[] myArray3 = new int[x];
+        
+        Random rand = new Random(1);
         
         MergeSort merge = new MergeSort();
         BottomUpMergeSort bmerge = new BottomUpMergeSort();
-        
+        MergeSort3Way merge3 = new MergeSort3Way();
         
         for (int i = 0; i < x; i++){ 
             myArray[i] = rand.nextInt(100);
             myArray2[i] = rand.nextInt(100);
+            myArray3[i] = rand.nextInt(100);
         }
         
         int start = (int) System.currentTimeMillis();
-        
         merge.Sort(myArray, 0, myArray.length - 1);     
-        
         int elapsedTime = (int) (System.currentTimeMillis() - start);
-
         System.out.println("Merge Sort: " + elapsedTime + " ms");
         
         start = (int) System.currentTimeMillis();
-        
-        merge.Sort(myArray, 0, myArray.length - 1);     
-        
+        bmerge.sort(myArray2);     
         elapsedTime = (int) (System.currentTimeMillis() - start);
-        
         System.out.println("Bottom up Merge Sort: " + elapsedTime + " ms");
         
+        start = (int) System.currentTimeMillis();
+        merge3.Sort3Way(myArray3);   
+        elapsedTime = (int) (System.currentTimeMillis() - start);
+        System.out.println("3 Way Merge Sort: " + elapsedTime + " ms");
     }
 }
+ 
